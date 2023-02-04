2c1651bf312533ec2dafd298eec0c4411e9e7435
Mohammad Hasifa <100508446+Hasifa1@users.noreply.github.com>
2023-02-04 18:41:29 +0530

    Delete MainApp directory

--- MainApp/seats_avail.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn=sqlite3.connect("theatre_db.db")
-from MainApp import insert_mv as im
-def check_seats(mid):
-
-    return im.seats_dict.get(mid)
--- MainApp/view_bookings.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("theatre_db.db")
-def get_bookings():
-    records = conn.execute('''select * from book_det''')
-    print(records)
-    for i in records:
-        print(i)
